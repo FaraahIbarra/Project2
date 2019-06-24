cf0b3b084c99b201ca437738a5f21f281f4f3e73
kpassaglia <kpassaglia@gmail.com>
2019-06-24 18:37:34 -0500

    config require path fixed

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require ('dotenv').config();
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 9000;
-var config = require('./config');
+var PORT = process.env.PORT || 8080;
+var config = require('./config/config');
 
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.listen(port);
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 
-//@TODO Delete below after you verify the the app is working
 app.route('/').get(function(request, response) {
     response.json(config);
 });
