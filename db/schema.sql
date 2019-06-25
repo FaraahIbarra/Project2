6ac31ec0b1b4fa10dca60183f69080d64840c7f6
aafzal <ambreenfafzal@gmail.com>
2019-06-24 20:43:05 -0500

    Added the Schema and tables

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+
+
+CREATE TABLE text_tb
+ 
+(id int NOT NULL AUTO_INCREMENT,
+Mood varchar(255) NOT NULL,
+content varchar(255) NOT NULL,
+PRIMARY KEY (id)
+)
+
+
+ 
+
+CREATE TABLE images
+(id int NOT NULL AUTO_INCREMENT,
+Mood varchar(255) NOT NULL,
+image longblob NOT NULL,
+PRIMARY KEY (id)
+)
+
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (task) VALUES ('Pick up milk.');
+INSERT INTO tasks (task) VALUES ('Mow the lawn.');
+INSERT INTO tasks (task) VALUES ('Call Shannon back.');
