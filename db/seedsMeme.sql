3fe0309cf3cbe3aa3cfd4408719dcb015f9c957c
patricia-dalinis <47152085+patricia-dalinis@users.noreply.github.com>
2019-07-05 21:39:37 -0500

    App functional, db populating, formatting good

--- db/seedsMeme.sql
+++ db/seedsMeme.sql
@@ -1,4 +1,3 @@
-INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('confused', '/assets/images/angrymeme1.jpg', 'blahhh', 'blahhhhhhhh');
-INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('confused', '/assets/images/angrymeme2.jpg', 'blahhh', 'blahhhhhhhh');
-INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('angry','/assets/images/sadmeme1.jpg', 'blahhh', 'blahhhhhhhh');
-INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('angry','/assets/images/sadmeme2.jpg', 'blahhh', 'blahhhhhhhh');
+INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('confused', '/assets/images/angry1.jpg', 'This is', 'lame');
+
+INSERT INTO memes_tb (mood, content, top_text, bottom_text) VALUES ('angry','/assets/images/sad1.jpg', 'lame?!', 'okay then...');
